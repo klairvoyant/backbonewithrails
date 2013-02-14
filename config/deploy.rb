4249982d5908ad0e3474d0cede31ddca1a4197ed
John <john@klairvoyant.com>
2013-02-14 19:50:33 +0530

    initial feb 14 3

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,44 +1,35 @@
-default_run_options[:pty] = true  # Must be set for the password prompt
-                                  # from git to work
-set :repository, "git@github.com:klairvoyant/backbonewithrails.git"  # Your clone URL
-set :scm, "git"
-set :user, "root"  # The server's user for deploys
-set :scm_passphrase, "billjo22"  # The deploy user's password
+require 'bundler/capistrano'
+set :user, 'root'
+set :domain, 'klairvoyant.in'
+set :applicationdir, "projectdir.git"
+set :scm_passphrase, "billjo22"
 
+set :scm, 'git'
+set :repository,  "ssh://root@klairvoyant.in/~/projectdir.git"
+set :git_enable_submodules, 1 # if you have vendored rails
+set :branch, 'master'
+set :git_shallow_clone, 1
+set :scm_verbose, true
+ssh_options[:forward_agent] = true
 
+# roles (servers)
+role :web, domain
+role :app, domain
+role :db,  domain, :primary => true
 
-#require 'bundler/capistrano'
-#set :user, 'root'
-#set :domain, 'localhost'
-#set :applicationdir, "backbone"
-#
-#
-#set :repository,  "git@github.com:klairvoyant/backbonewithrails.git"
-## set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-## Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-#
-#role :web, domain                          # Your HTTP server, Apache/etc
-#role :app, domain                          # This may be the same as your `Web` server
-#role :db,  "localhost:3306", :primary => true # This is where Rails migrations will run
-##role :db,  "your slave db-server here"
-#
-## if you want to clean up old releases on each deploy uncomment this:
-# #after "deploy:restart", "deploy:cleanup"
-#
-## deploy config
-#set :deploy_to, applicationdir
-#set :deploy_via, :export
-#
-#default_run_options[:pty] = true
+# deploy config
+set :deploy_to, applicationdir
+set :deploy_via, :export
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+# additional settings
+default_run_options[:pty] = true  # Forgo errors when deploying from windows
+ssh_options[:keys] = %w(/home/john/.ssh/id_rsa)            # If you are using ssh_keysset :chmod755, "app config db lib public vendor script script/* public/disp*"set :use_sudo, false
 
-# If you are using Passenger mod_rails uncomment this:
+# Passenger
 namespace :deploy do
-   task :start do ; end
-   task :stop do ; end
-   task :restart, :roles => :app, :except => { :no_release => true } do
-     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-   end
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
 end
